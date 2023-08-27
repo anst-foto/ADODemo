cc58f2849ef60732d4e3c2f54de4451b38dc244f
Starinin Andrey <starininandrey@gmail.com>
2023-08-27 13:55:37 +0300

    Update

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -33,3 +33,52 @@ INSERT INTO table_users (first_name, last_name, email) VALUES ('user', 'user', '
 
 INSERT INTO table_accounts (login, password, role_id) VALUES ('admin', '123', (SELECT id FROM table_roles WHERE name = 'admin'));
 INSERT INTO table_users (first_name, last_name, email) VALUES ('admin', 'user', 'admin@mail.mail');
+
+CREATE PROCEDURE procedure_insert_user (IN login_arg TEXT, IN password_arg TEXT, IN role_name_arg TEXT, IN first_name_arg TEXT, IN last_name_arg TEXT, IN email_arg TEXT)
+LANGUAGE SQL
+BEGIN ATOMIC
+    INSERT INTO table_accounts (login, password, role_id) VALUES (login_arg, password_arg, (SELECT id FROM table_roles WHERE name = role_name_arg));
+    INSERT INTO table_users (first_name, last_name, email) VALUES (first_name_arg, last_name_arg, email_arg);
+END;
+
+-- CALL procedure_insert_user('admin', '12345', 'admin', 'admin', 'admin', 'adm@adm');
+
+CREATE VIEW view_acoounts AS
+SELECT last_name, first_name,
+       login, password,
+       table_roles.name AS role_name,
+       email
+FROM table_users
+JOIN table_accounts
+    ON table_users.id = table_accounts.id
+JOIN table_roles
+    ON table_accounts.role_id = table_roles.id;
+
+-- LOG
+CREATE TABLE table_log (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    table_name TEXT NOT NULL,
+    action_name TEXT NOT NULL,
+    comment TEXT NULL
+);
+
+CREATE PROCEDURE procedure_log(IN table_name_arg TEXT, IN action_name_arg TEXT, IN comment_arg TEXT)
+LANGUAGE SQL
+BEGIN ATOMIC
+    INSERT INTO table_log(table_name, action_name, comment) VALUES (table_name_arg, action_name_arg, comment_arg);
+END;
+
+CREATE FUNCTION function_log_for_table_roles()
+RETURNS trigger AS
+$$
+    BEGIN
+        CALL procedure_log('table_roles', 'insert', concat(NEW.id, ' ', NEW.name));
+        RETURN NEW;
+    END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_log_for_table_roles
+AFTER INSERT ON table_roles
+FOR EACH ROW
+EXECUTE FUNCTION function_log_for_table_roles();
